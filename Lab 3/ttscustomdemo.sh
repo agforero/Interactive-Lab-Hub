69654fb85421e818e2eb3a1dfe49e3c89aef0c63
Agustin Forero <agforero@gmail.com>
2021-10-04 22:25:27 -0400

    Lab 3 Part 1: files

---
+++ Lab 3/ttscustomdemo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Hello Agustin, I'm probably going to mispronounce your name, but I want you to know that you're extremely handsome and smart and attractive and funny. And humble and have a really high IQ" | festival --tts
