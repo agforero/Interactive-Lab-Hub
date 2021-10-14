840a543a51ce32c60d19f5551a94e323f01cf956
Agustin Forero <agforero@gmail.com>
2021-10-13 20:56:13 -0400

    sort of kind of making it work

--- Lab 3/weatherbot/input.sh
+++ Lab 3/weatherbot/input.sh
@@ -1,2 +1,3 @@
+#arecord -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
 arecord -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
-python3 test_words.py recorded_mono.wav
+python3 main.py recorded_mono.wav
